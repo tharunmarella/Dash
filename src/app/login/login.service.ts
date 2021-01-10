616910b33b4658c915a5949837a4082a5865d938
EC2 Default User <ec2-user@ip-172-31-89-115.ec2.internal>
2021-01-10 05:35:08 +0000

    aws

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -13,10 +13,28 @@ export class LoginService {
   postRequest(user,pass){
     const headers = { 'Content-Type': 'application/json'};
     const body = { "email": user,"password": pass };
-    this.http.post<any>('v1/login', body, { headers }).subscribe(data => {
-        console.log(data);
-        // this.storage.set("accessKey","hello");
-    });
+    
+
+    this.http.post('http://54.164.171.220:8080/api/v1/login',{"email": "tharunmarella@mail.fresnostate.edu","password": "12345678"})
+      .subscribe(
+        res => {
+          this.id=res.id;
+          console.log(res);
+        },
+        err => {
+          console.log("Error occured");
+        }
+      );
+    
+
+
+
+
+
+    // this.http.post<any>('http://54.164.171.220:8080/api/v1/login', body, { headers }).subscribe(data => {
+    //     console.log(data);
+    //     // this.storage.set("accessKey","hello");
+    // });
 
   }
 }
